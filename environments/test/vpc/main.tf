600be49851320eb51c3ae3b7074bf3f6d29e09d0
walkable-llc <167944030+rzkw@users.noreply.github.com>
2026-09-18 16:34:03 +0700

    Rename VPC and resources in main.tf

--- environments/test/vpc/main.tf
+++ environments/test/vpc/main.tf
@@ -2,15 +2,15 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_vpc" "this" {
+resource "aws_vpc" "vpc-1" {
   cidr_block = var.vpc_cidr
 }
 
-resource "aws_subnet" "this" {
+resource "aws_subnet" "dev" {
   vpc_id     = aws_vpc.this.id
   cidr_block = var.subnet_cidr
 }
 
-resource "aws_route_table" "this" {
+resource "aws_route_table" "rt-1" {
   vpc_id = aws_vpc.this.id
 }
