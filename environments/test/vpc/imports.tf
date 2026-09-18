8aec8210f41842e0443d097f6d0e602eaaed7672
walkable-llc <167944030+rzkw@users.noreply.github.com>
2026-09-18 16:34:58 +0700

    Update import targets in imports.tf

--- environments/test/vpc/imports.tf
+++ environments/test/vpc/imports.tf
@@ -1,14 +1,14 @@
 import {
-  to = aws_vpc.this
+  to = aws_vpc.vpc-1
   id = var.vpc_id
 }
 
 import {
-  to = aws_subnet.this
+  to = aws_subnet.dev
   id = var.subnet_id
 }
 
 import {
-  to = aws_route_table.this
+  to = aws_route_table.rt-1
   id = var.route_table_id
 }
